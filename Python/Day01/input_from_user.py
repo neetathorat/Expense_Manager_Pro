328806a5e495a5765dd026be394dbecb9e390f09
neetathorat <neeta.thorat1@gmail.com>
2026-07-17 23:05:40 +0530

    Lesson 1: Variables, data types, input and output

---
+++ Python/Day01/input_from_user.py
@@ -0,0 +1,7 @@
+name = input("Enter name : ")
+age = int(input("Enter age : "))
+dream_job = input("Enter Dream Job : ")
+
+print("Name      : ",name)
+print("Age       : ",age)
+print("Dream Job : ",dream_job)
---
+++ Python/Day01/variable_print.py
@@ -0,0 +1,7 @@
+name = "Neeta"
+age = 20
+dream_job = "Python AI Automation Developer"
+
+print("Name      : ",name)
+print("Age       : ",age)
+print("Dream Job : ",dream_job)
